231525a3208b72b253d9446bdc335d7a62eb2105
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-04-17 15:28:48 +0200

    Update: initial cleanup & configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    ssr: {
+      external: ['node:async_hooks'],
+    },
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
