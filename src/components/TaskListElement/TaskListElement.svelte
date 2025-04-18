3d5d1eb56f46ebab632a82eafb6573395ca69d0a
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-04-18 16:45:01 +0200

    Update: show the expiration date next to the task title

--- src/components/TaskListElement/TaskListElement.svelte
+++ src/components/TaskListElement/TaskListElement.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
   import PriorityElement from 'components/PriorityElement.svelte';
   import CheckboxElement from 'components/CheckboxElement.svelte';
+    import { ClockHistory } from 'svelte-bootstrap-icons';
 
   let props = $props();
 </script>
@@ -28,11 +29,21 @@ data-testid='container-main-task'>
   <div class='w-full pl-2 
   border-l border-sky-500
   grid grid-rows-2'>
-    <div class='w-full h-full overflow-hidden'>
-      <p class='
+    <div class='w-full h-full 
+    flex gap-2 overflow-hidden'>
+      <p class='grow
       text-sky-500 font-bold text-left whitespace-nowrap text-ellipsis overflow-hidden'>
         {props?.task?.title ? props?.task?.title : 'Unnamed'}
       </p>
+
+      {#if props?.task?.expiresAt}
+        <p class='shrink-0 
+        flex
+        text-sky-500 text-left text-sm whitespace-nowrap text-ellipsis overflow-hidden'>
+          <ClockHistory class='h-3 w-3 mt-1.25 mr-1' />
+          {new Date(props?.task?.expiresAt * 1000).toISOString().slice(0, 10)}
+        </p>
+      {/if}
     </div>
 
     <div class='w-full h-full overflow-hidden'>
